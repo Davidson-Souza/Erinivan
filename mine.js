530128ebdae3223f18c2da5a3a702d4de5c12b0e
Davidson Souza <davidson.lucas.souza@outlook.com>
2021-07-31 05:44:02 +0300

    remove done todo

--- mine.js
+++ mine.js
@@ -1,56 +1,24 @@
-const { exec } = require('child_process');
 var isRunning = false, players = 0;
-
+const axios = require('axios').default;
+const client = axios.create(
+  {
+    baseURL:"http://localhost:8080"
+  });
 module.exports = 
 {
-  start: (onSuccess, onClose, onError, client) =>
+  start: async () =>
   {
-    if(isRunning == true) return ;
-    isRunning = true;
-    const ls = exec(`${process.env.SERVER}`);
-    ls.stdout.on('data', (data) => {
-      if(data.includes("joined"))
-      {
-        players += 1;
-        client.user.setActivity(`${process.env.HOST}: ${players} online`);
-
-      } else if (data.includes("Disconnected"))
-      {
-        players -= 1;
-        if(players == 0)
-        {
-          setTimeout(() =>
-          {
-            stop();
-          }, 20000)}
-        client.user.setActivity(`${process.env.HOST}: ${players} online`);
-
-      } else if(data.includes("Done"))
-      {
-        client.user.setActivity(`${process.env.HOST}: ${players} online`);
-        onSuccess();
-      }
-    });
-    
-    ls.stderr.on('data', (data) => {
-      isRunning = false;
-      client.user.setActivity(`Cala boca Pedro`);
-      if(onError) onError();
-    });
-    
-    ls.on('close', (code) => {
-      client.user.setActivity(`Cala boca Pedro`);
-      onClose();
-    });
+    const res = await client.get(`/`);
+    return res.data;
   },
-  getAddress: () =>
+  getAddress: async () =>
   {
-    return process.env.HOST
+    const res = await client.get(`/`);
+    return res.data.res;
   },
   stop: async () =>
   {
-    isRunning = false;
-    /**@todo: ls.send("stop") */
-    await exec("kill -15 $(pidof java)");
+    const res = await client.get(`/`);
+    return res.data.res;
   }
 }
